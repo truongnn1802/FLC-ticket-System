dbd2061f83e85f7748fbcdb037f21290393aa854
hieuvdt <hieuvdt@fpt.com.vn>
2023-02-16 15:48:01 +0700

    update content page tao yeu cau moi

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -4,14 +4,12 @@ import Images from 'src/assets/images/404.jpg'
 
 const NotFoundPage = () => {
   return (
-    <DefaultLayout>
     <section className='container min-height'>
       <div className={styles.notfound} style={{ backgroundImage: `url(${Images})` }}></div>
       <div className={styles.notification}>
-      <h2>Rất tiếc chúng tôi không tìm thấy nội dung của trang này !</h2>
+        {/* <h2>Rất tiếc chúng tôi không tìm thấy nội dung của trang này !</h2> */}
       </div>
-      </section>
-    </DefaultLayout>
+    </section>
   )
 }
 export default NotFoundPage
